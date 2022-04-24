314bc6ed0d9fe6ce778dedbf0672449cbfd66e65
Olaosebikan Israel <daerudite01@gmail.com>
2022-04-24 21:58:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose= require('mongoose');
+const {dbConnect}= require('./src/config/database')
 const productRoutes=require('./src/api/routes/product.js')
 
 const morgan=require('morgan')
@@ -10,25 +10,17 @@ const morgan=require('morgan')
 // create express app
 const app = express();
 
+//connect database
+dbConnect(app);
+
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
-
-
-const Private_Key=process.env.dbURI
-mongoose.connect(Private_Key,{ useNewUrlParser: true, useUnifiedTopology: true  })
-.then((result)=> app.listen(3000))
-.catch((error)=> console.log('mongoose error occurred'));
-
-
-
 app.get('/',(req,res)=>{
     res.redirect('/products');
 })
 
 app.use('/' , productRoutes)
-
-
